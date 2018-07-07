9f5f5d45d1fffd51863e44579f69debb611934ea
Jacob Packert <jacobpackert@gmail.com>
2018-07-07 15:57:15 +0200

    added fetch and state to the App component to pass fetch data to child components as props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 // import './App.css';
 import NewButton from './components/NewButton';
+import WorkoutDescription from './components/WorkoutDescription';
+
+
+  // Attempt to use the fetch API. Should be moved somewhere it makes sense, so that the button onClick will fetch data and send as props to components with workout description
+      let url = "https://spreadsheets.google.com/feeds/list/1hwe64eKPIU7W-jvzFWR8MUzW9DrQerp67G7QBVxvdjk/od6/public/values?alt=json";
+      let wod = "100 PUSHUPS";
+
+
+
 
 
 // The app component
 class App extends Component {
-    render() {
+  constructor(){
+    super();
+    this.state = {
+      dataContent: [],
+    };
+  }
+
+
+  // Writing the fetch
+  componentDidMount(){
+
+    fetch(url).then(
+          response => {
+            if (response.status !== 200) {
+              console.log("there is a problem!! " + response.status);
+              return;
+            }
+
+            response.json()
+            .then(data => {
+              console.log("data: " + data.feed.entry[0].gsx$description.$t);
+              // console.log("response entry: " + data.feed.entry[0].gsx$description.$t);
+              // let dataContent = data.feed.entry[0].gsx$description.$t;
+              // console.log("dataContent: " + dataContent);
+              this.setState({ dataContent: data.feed.entry[0].gsx$description.$t })              
+            });
+          }
+        )
+        .catch(function (err) {
+          console.log("Fetch error: ", err);
+        });
+  }
+  
+  
+  render() {
       return (
       <div className="App">
         <header className="App-header">
@@ -14,6 +57,7 @@ class App extends Component {
         <p className="App-intro">
           This is going to be a cool project. Stay tuned..
         </p>
+        <WorkoutDescription value={this.state.dataContent}/>
         < NewButton value="New WOD 💪" />
       </div>
       );
@@ -21,27 +65,7 @@ class App extends Component {
   };
   
 
-  // Attempt to use the fetch API. Should be moved somewhere it makes sense, so that the button onClick will fetch data and send as props to components with workout description
 
-  let url = "https://spreadsheets.google.com/feeds/list/1hwe64eKPIU7W-jvzFWR8MUzW9DrQerp67G7QBVxvdjk/od6/public/values?alt=json";
-
-  fetch(url).then(
-    function(response){
-      if(response.status !== 200){
-        console.log("there is a problem!! " + response.status);
-        return;
-      }
-
-    response.json().then(function (data) {
-    console.log("data: " + data);
-    console.log("response entry: " + data.feed.entry[0].gsx$description.$t);
-
-      });
-    }     
-  )
-  .catch(function(err) {
-    console.log("Fetch error: ", err);
-  });
   
 
 export default App;
---
+++ src/components/WorkoutDescription.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WorkoutDescription extends React.Component {
+    render () {
+        return (
+            <p>{this.props.value}</p>
+        )
+    }
+};
+
+
+
+export default WorkoutDescription;
