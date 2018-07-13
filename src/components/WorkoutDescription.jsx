e22ecfb87d77a662d79d3c5781b8911c91d140bb
Jacob Packert <jacobpackert@gmail.com>
2018-07-13 12:19:14 +0200

    changed workoutdescription component to fix div p mixup

--- src/components/WorkoutDescription.jsx
+++ src/components/WorkoutDescription.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class WorkoutDescription extends React.Component {
     render () {
         return (
-            <p><div dangerouslySetInnerHTML={{__html: this.props.wodContent}}></div></p>
+            <div><p dangerouslySetInnerHTML={{__html: this.props.wodContent}}></p></div>
         )
     }
 };
