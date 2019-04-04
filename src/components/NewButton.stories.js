596984fd11c009066245562de35bce4e1333d574
Jacob Packert <jacobpackert@gmail.com>
2019-04-04 20:43:53 +0200

    updated story to fix NewButton function problem

--- src/components/NewButton.stories.js
+++ src/components/NewButton.stories.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import NewButton from './NewButton';
 
 const emojiArray = ["💪", "🏋️‍♂️", "🏃‍♂️"];
 
 storiesOf('NewButton', module)
-    .add('default', () => <NewButton emoji = {emojiArray} buttonContent={"New WOD"}></NewButton> );
+    .add('default', () => 
+    <NewButton
+        onButtonClick={action('clicked')}
+        emoji = {emojiArray}
+        buttonContent={"New WOD"}>
+    </NewButton> );
